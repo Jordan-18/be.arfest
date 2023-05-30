c6cef39932e8b3e8c370509c13f88b92da5be727
Jordan-18 <jordanistiqlal18@gmail.com>
2023-05-30 18:02:23 +0700

    checkpoint gauth

---
+++ Modules/GAuth/Http/Services/GauthService.php
@@ -0,0 +1,14 @@
+<?php
+namespace Modules\Gauth\http\Services;
+
+class GAuthService{
+    public function loginGoogle()
+    {
+        # code...
+    }
+
+    public function registerGoogle()
+    {
+        # code...
+    }
+}
